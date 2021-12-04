c37b1d4fcf2b03fa04dfac5d6df7f9c12b42b089
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2021-12-04 18:37:06 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f1962cd53c34770ad6eb419011c50384'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8045b135-efdc-448b-a5fc-b6a4ff5156f9'
+};
